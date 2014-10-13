a45784a1b94c7371b472bd401959212ad9e358c3
James.Carpenter <James.Carpenter@MU-C3-10.mullen.pvt.k12.co.us>
2014-10-13 14:41:31 -0600

    constructor called product

--- Vending Machine Group Project/src/Product.java
+++ Vending Machine Group Project/src/Product.java
@@ -1,5 +1,47 @@
 
 public class Product
 	{
+	private int price;
+	private String snackname;
+	private int quantity;
+	
+	public Product(int p, String sn, int q)
+		{
+		price=p;
+		snackname=sn;
+		quantity=q;
+		}
+
+	public int getPrice()
+		{
+		return price;
+		}
+
+	public void setPrice(int price)
+		{
+		this.price = price;
+		}
+
+	public String getSnackname()
+		{
+		return snackname;
+		}
+
+	public void setSnackname(String snackname)
+		{
+		this.snackname = snackname;
+		}
+
+	public int getQuantity()
+		{
+		return quantity;
+		}
+
+	public void setQuantity(int quantity)
+		{
+		this.quantity = quantity;
+		}
+
+	
 
 	}

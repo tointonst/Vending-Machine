62b8eca4882df421fe58c3e11bf91c4b615e73a2
Harris.Butler <Harris.Butler@MU-C3-12.mullen.pvt.k12.co.us>
2014-10-13 14:59:50 -0600

    test

--- Vending Machine Group Project/src/Cashier.java
+++ Vending Machine Group Project/src/Cashier.java
@@ -1,5 +1,13 @@
-
-public class Cashier
-	{
-
-	}
+import java.util.Scanner; 
+	public class Cashier
+		{
+		private int currentCents; 
+		
+			public int[] addMoney(int value) 
+			{
+				
+				System.out.println("Please insert money");
+				Scanner userInput1 = new Scanner(System.in);
+				int value = userInput1.nextInt(); 
+			}
+		}

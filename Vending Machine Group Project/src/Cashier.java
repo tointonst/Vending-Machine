6c1f5de8dd53d75e0d019ff25fb36c98bc189159
Harris.Butler <Harris.Butler@MU-C3-12.mullen.pvt.k12.co.us>
2014-10-14 13:56:40 -0600

    adding cashier

--- Vending Machine Group Project/src/Cashier.java
+++ Vending Machine Group Project/src/Cashier.java
@@ -5,9 +5,20 @@ import java.util.Scanner;
 		
 			public int[] addMoney(int value) 
 			{
-				
-				System.out.println("Please insert money");
+				System.out.println("Please insert money. ex Quarters, Dimes, Nickels, Pennies");
 				Scanner userInput1 = new Scanner(System.in);
-				int value = userInput1.nextInt(); 
+				int money = userInput1.nextInt();
+				
+				for(int i = 0; i < 10; i++) 
+					{
+						if(userInput1.equals("Quarter") || userInput1.equals("quarter")) 
+						{
+							value = value + 25; 
+						}
+						if(userInput1.equals("Dime") || userInput1.equals("dime")) 
+						{
+							value = value + 10; 
+						}
+					} 
 			}
 		}

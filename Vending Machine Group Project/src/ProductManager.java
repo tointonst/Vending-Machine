dfc46aca718d32d1df64b12b7796dd1bb9463265
steven.tointon <steven.tointon@MU-C3-11.mullen.pvt.k12.co.us>
2014-10-16 14:24:19 -0600

    added prodcut as some perameter

--- Vending Machine Group Project/src/ProductManager.java
+++ Vending Machine Group Project/src/ProductManager.java
@@ -1,33 +1,84 @@
 public class ProductManager
 	{
-	private String product;
+	
 	private String itemCode;
+	private static String nextItemCode = "A1";
+	private Product product;
+	private int quantity;
 
-	public ProductManager(String p, String ic)
+	public ProductManager(Product p, int q )
 		{
 		product = p;
-		itemCode = ic;
+		quantity = q;
 		}
 
-	public String getProduct()
-		{
-		return product;
-		}
-
-	public void setProduct(String product)
-		{
-		this.product = product;
-		}
+	
 
 	public String getItemCode()
 		{
 		return itemCode;
 		}
 
+
+
 	public void setItemCode(String itemCode)
 		{
 		this.itemCode = itemCode;
 		}
+
+
+
+	public static String getNextItemCode()
+		{
+		return nextItemCode;
+		}
+
+
+
+	public static void setNextItemCode(String nextItemCode)
+		{
+		ProductManager.nextItemCode = nextItemCode;
+		}
+
+
+
+	public Product getProduct()
+		{
+		return product;
+		}
+
+
+
+	public void setProduct(Product product)
+		{
+		this.product = product;
+		}
+
+
+
+	public int getQuantity()
+		{
+		return quantity;
+		}
+
+
+
+	public void setQuantity(int quantity)
+		{
+		this.quantity = quantity;
+		}
+	
+
+
+
+	@Override
+	public String toString()
+		{
+		return "ProductManager [itemCode=" + itemCode + ", product=" + product
+				+ ", quantity=" + quantity + "]";
+		}
+
+
 
 	public boolean isInStock()
 		{

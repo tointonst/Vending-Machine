e034fc38986175de3310c066f632b3a97c182c00
steven.tointon <steven.tointon@MU-C3-11.mullen.pvt.k12.co.us>
2014-10-16 14:13:41 -0600

    added methods inStock and vendItem to product manager -Steve

--- Vending Machine Group Project/src/ProductManager.java
+++ Vending Machine Group Project/src/ProductManager.java
@@ -1,31 +1,63 @@
-
-
 public class ProductManager
 	{
 	private String product;
 	private String itemCode;
+
 	public ProductManager(String p, String ic)
 		{
-		product=p;
-		itemCode=ic;
+		product = p;
+		itemCode = ic;
 		}
+
 	public String getProduct()
 		{
 		return product;
 		}
+
 	public void setProduct(String product)
 		{
 		this.product = product;
 		}
+
 	public String getItemCode()
 		{
 		return itemCode;
 		}
+
 	public void setItemCode(String itemCode)
 		{
 		this.itemCode = itemCode;
 		}
-	
-	}
 
+	public boolean isInStock()
+		{
+		if (InventoryManger.quantity > 0)
+			{
+			return true;
+			}
+
+		else
+			{
+			return false;
+			}
+		}
+
+	public boolean vendItem()
+		{
+		if (isInStock()== true)
+			{
+			InventoryManger.quantity = InventoryManger.quantity-1;
+			return true;
+			}
+		else
+			{
+			return false;
+			}
+		}
 	
+	public static void incrementItemCode()
+		{
+		
+		}
+
+	}
